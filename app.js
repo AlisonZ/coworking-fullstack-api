26be0dc9b9c82d178902eb91e2cecabf01c28ead
Alison Zerbe <alisonkzerbe@gmail.com>
2019-07-23 21:34:41 -0700

    working with MongoDb

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const { NODE_ENV ,PORT } = process.env;
+const { MONGO_DB, NODE_ENV ,PORT } = process.env;
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
+
+if (MONGO_DB) {
+    mongoose.connect(MONGO_DB, {useNewUrlParser: true});
+    console.log('connected to mongoose');
+} else {
+    console.log('not connected to mongo database');
+}
 
 if(NODE_ENV === 'development') app.use(morgan('dev'));
 
