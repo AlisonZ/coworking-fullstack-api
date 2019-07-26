f3cbae32640de85edd97eb6fee504f3155cbf686
Alison Zerbe <alisonkzerbe@gmail.com>
2019-07-25 18:59:27 -0700

    GET working for first 3 calls

--- api/routes/units.js
+++ api/routes/units.js
@@ -36,44 +36,6 @@ router.get('/', async (req, res, next) => {
 
 });
 
-router.patch('/:id', async(req, res, next) => {
-    const status = 201;
-    const filter = { _id: req.params.id};
-    const update = req.body;
-
-    let response = await Units.findOneAndUpdate(filter, update, {
-        new: true
-    });
-
-    res.json({ status, response });
-    
-        //TODO: add 404 if ID not found
-});
-
-router.patch('/:id/company', async (req, res, next) => {
-    const status = 201;
-    // const filter = { _id: req.params.id};
-    const update = req.body;
-    // console.log('reqqqq', req.body);
-
-   
-
-    const { company } = await Units.findById(req.params.id).select('company');    
-    // console.log('comppp', company);
-    
-
-    const response = await Units.findOneAndUpdate(company, update);
-
-  
-
-    // const response = await Units.findById(req.params.id);
-
-    res.json({ status, response });
-
-
-
-    //TODO: add 404 if ID not found
-});
 
 //this is for me to get some data in db to play with
 router.post('/', async (req, res, next) => {
@@ -82,8 +44,6 @@ router.post('/', async (req, res, next) => {
 
     const response = await Units.create(req.body);
     res.json({ status, response});
-
-
 });
 
 module.exports = router;
