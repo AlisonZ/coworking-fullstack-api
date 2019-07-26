e85ab584534db4b215b751958d28000afd11370b
Alison Zerbe <alisonkzerbe@gmail.com>
2019-07-25 18:16:37 -0700

    updates units model

--- api/models/units.js
+++ api/models/units.js
@@ -16,50 +16,22 @@ const schema = new Units({
         type: Number,
         required: false
     },
-    //TODO: rework the routing logic so that this field is not necessary
-    //instead of having an explicit field of 'occupied' can base this logic off of
-    //the presence or absence of a company name in the company section
-    //maybe? but why is there an array of companies per one unit?
-    occupied: {
-      type: String, 
-      required: false
-    },
-    // //Q: should this be a model itself?
-    company: 
-        {
-            name: {
-                type: String, 
-                required: true
-            }, 
-            contact_email: {
-                type: String, 
-                required: true
-            },
-            // Q: should this be a model in itself?
-            employees: [
-                {
-                    first_name: {
-                        type: String, 
-                        required: true
-                    },
-                    last_name: {
-                        type: String, 
-                        required: true
-                    }, 
-                    preferred_name: String, 
-                    position: String, 
-                    date_of_birth: String, 
-                    //TODO: validate email 
-                    email: {
-                        type: String, 
-                        required: true
-                    }
-                }
-            ]
-        }
-    ,
-  }
-// }, {createdAt: 'created_at', updatedAt: 'updated_at'}
+
+    //this might not need to be an array because there is only one company in each unit at this time
+    company:[{
+      name: {type: String, required: true}, 
+      contact_email: {type: String, required: true},
+      employees: [{
+        first_name: {type: String,required: true},
+        last_name: {type: String,required: true}, 
+        preferred_name: String, 
+        position: String, 
+        date_of_birth: String, 
+        //TODO: validate email 
+        email: {type: String, required: true}
+      }]
+    }] 
+  }, {createdAt: 'created_at', updatedAt: 'updated_at'}
 );
 
 module.exports = mongoose.model('Units', schema);
