1aebe1f552606cb0db3882e6e1e79ba710d86761
Alison Zerbe <alisonkzerbe@gmail.com>
2019-07-26 16:16:08 -0700

    starts to add DELETE for company

--- api/routes/units.companies.js
+++ api/routes/units.companies.js
@@ -8,4 +8,13 @@ router.get('/', async(req, res, next) => {
     res.json({ status, companies });
 });
 
+router.delete('/', async(req, res, next) => {
+    const status = 200;
+    const unit = await Units.findById(req.params.unitId);
+    const company = unit.companies.remove();
+    unit.save();
+
+    res.json({ status, company });
+});
+
 module.exports = router;
