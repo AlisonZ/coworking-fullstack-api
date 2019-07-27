35ddb314c28767abdf326b1555b127e3880a60ee
Alison Zerbe <alisonkzerbe@gmail.com>
2019-07-27 13:41:54 -0700

    cleans up company routes

--- api/routes/units.company.js
+++ api/routes/units.company.js
@@ -3,42 +3,62 @@ const Units = require('../models/units');
 
 router.get('/', async(req, res, next) => {
     const status = 200;
-    const { companies } = await Units.findById(req.params.unitId);
+    const { company } = await Units.findById(req.params.unitId);
 
-    res.json({ status, companies });
+    res.json({ status, company});
 });
 
 router.post('/', async(req, res, next) => {
-    const status = 201;
-    const unit = await Units.findById(req.params.unitId);
+    try {
 
-    unit.companies = req.body;
-    await unit.save();
-    
-    const company = unit.companies;
-
-    res.json({ status, company });
-});
+        const status = 201;
+        const unit = await Units.findById(req.params.unitId);
+        
+        unit.company= req.body;
+        await unit.save();
+        
+        const company = unit.company;
+        
+        res.json({ status, company });
+    } catch (error) {
+        error.status = 404;
+        error.message = `${req.method} ${req.path} failed. Unit id could not be found`;
+        next(error);
+    }
+}); 
 
 router.patch('/', async(req, res, next) => {
-    const status = 201;
-    const unit = await Units.findById(req.params.unitId);
-    const company = unit.companies[0];
-    Object.assign(company, req.body);
-    await unit.save();
+    try {
 
-    res.json({ status, unit });
+        const status = 201;
+        const unit = await Units.findById(req.params.unitId);
+        const company = unit.company[0];
+        Object.assign(company, req.body);
+        await unit.save();
+        
+        res.json({ status, unit });
+    } catch (error) {
+        error.status = 404;
+        error.message = `${req.method} ${req.path} failed. Unit id could not be found`;
+        next(error);
+    }
 });
 
 router.delete('/', async(req, res, next) => {
-    const status = 200;
-    const unit = await Units.findById(req.params.unitId);
+    try {
 
-    const company = unit.companies[0].remove();
-
-    unit.save();
-
-    res.json({ status, company });
+        const status = 200;
+        const unit = await Units.findById(req.params.unitId);
+        
+        const company = unit.company[0].remove();
+        
+        unit.save();
+        res.json({ status, unit });
+    } catch(error) {
+        error.status = 404;
+        error.message = `${req.method} ${req.path} failed. Unit id could not be found`;
+        next(error);
+    }
 });
 
 module.exports = router;
