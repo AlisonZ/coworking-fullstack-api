50ddcff6ba792a03d988e340748228ef49cb0687
Alison Zerbe <alisonkzerbe@gmail.com>
2019-07-27 13:58:22 -0700

    adds some error handling

--- api/routes/units.company.employees.js
+++ api/routes/units.company.employees.js
@@ -2,33 +2,54 @@ const router = require('express').Router({ mergeParams: true });
 const Units = require('../models/units');
 
 router.get('/', async(req, res, next) => {
-    const status = 200;
+    try {
+        const status = 200;
+        
+        const { company } = await Units.findById(req.params.unitId);
+        const employees = company[0].employees;   
 
-    //this destructuring is not working because companies needs to have the [0] 
-    // const {companies: {employees} } = await Units.findById(req.params.unitId);
+        res.json({ status, employees });
+    } catch (error) {
+        error.status = 404;
 
-    const { companies } = await Units.findById(req.params.unitId);
-    const employees = companies[0].employees;
-
-    res.json({ status, employees });
+        if (!company[0]) {
+            error.message = `${req.method} ${req.path} failed. No company listed for this unit`
+        } else if (!company) {
+            error.message = `${req.method} ${req.path} failed. No unit listed with this id`
+        }
+        next(error);
+    }
 });
 
 router.get('/:id', async(req, res, next) => {
-    const status = 200; 
-    const { companies } = await Units.findById(req.params.unitId);
-    const employee = await companies[0].employees.id(req.params.id)
+    try {
 
-    res.json({ status, employee });
+        const status = 200; 
+        const { company } = await Units.findById(req.params.unitId);
+        const employee = await company[0].employees.id(req.params.id)
+        
+        res.json({ status, employee });
+    } catch (error) {
+        error.status = 404;
+        if(!company) {
+            error.message = `${req.method} ${req.path} failed. There is no company listed for the unit you entered`
+        }
+
+        if(!employee) {
+            error.message = `${req.method} ${req.path} failed. There is no employee listed for the employee id you entered`   
+        }
+        next(error);
+    }
 }); 
 
 router.post('/', async(req, res, next) => {
     const status = 201;
     const unit = await Units.findById(req.params.unitId);
-    unit.companies[0].employees.push(req.body);
+    unit.company[0].employees.push(req.body);
 
     await unit.save();
 
-    const employee = unit.companies[0].employees[unit.companies[0].employees.length -1]
+    const employee = unit.company[0].employees[unit.company[0].employees.length -1]
 
     res.json({ status, employee });
 });
